9c23399d86382a97f54665487c92aa7cec607ff5
Zackary Richards <zackaryr@terpmail.umd.edu>
2020-03-31 15:55:46 -0400

    added button functionality

--- asst_2/main.js
+++ asst_2/main.js
@@ -17,12 +17,14 @@ images.forEach(image => {
 })
 
 /* Wiring up the Darken/Lighten button */
-btn.onClick("click", ()=> {
-    let button_class = btn.getAttribute("class");
-    console.log(button_class);
-    if(button_class === "dark") {
-        btn.setAttribute("class", "light");
-        btn.style.color = "";
-        btn.style.backgroundColor = "rgba(0,0,0,0.5)";
+btn.onclick = ()=>{
+    if (btn.getAttribute('class') == 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.innerHTML = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else if (btn.getAttribute('class') == 'light') {
+        btn.setAttribute('class', 'dark');
+        btn.innerHTML = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
-})
+}
